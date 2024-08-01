78fc61e34d958ca3fe22b1514a0f2f2596c9daed
Netanel Mizrahi <nmiz1987@gmail.com>
2024-08-01 17:48:23 +0300

    add style

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function MealPage() {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <section className={styles.main}></section>
     </>
   );
 }
